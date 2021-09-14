f0c8b0e24182d9b6784bed5d0c7bd1044436cca6
Hui Fang Yeo <hfy@activeviam.com>
2021-09-14 14:20:50 +0800

    Outputting dataframe and csv instead of std out

--- datagen_transaction.py
+++ datagen_transaction.py
@@ -6,225 +6,182 @@ import json
 import numpy as np
 import sys
 import datetime
-from datetime import timedelta
-from datetime import date
+from datetime import timedelta, datetime, date
 import math
 from random import sample
 from random import randint
 from faker import Faker
 
-
 import profile_weights
 
-def get_user_input():
-    # convert date to datetime object
-    def convert_date(d):
-        for char in ['/', '-', '_', ' ']:
-            if char in d:
-                d = d.split(char)
-                try:
-                    return date(int(d[2]), int(d[0]), int(d[1]))
-                except:
-                    error_msg(3)
-        error_msg(3)
 
-    # error handling for CL inputs
-    def error_msg(n):
-        if n == 1:
-            print('Could not open customers file\n')
-        elif n == 2:
-            print('Could not open main config json file\n')
-        else:
-            print('Invalid date (MM-DD-YYYY)')
-        output = 'ENTER:\n(1) Customers csv file\n'
-        output += '(2) profile json file\n'
-        output += '(3) Start date (MM-DD-YYYY)\n'
-        output += '(4) End date (MM-DD-YYYY)\n'
-        print(output)
-        sys.exit(0)
-
-    try:
-        customers = open(sys.argv[1], 'r').readlines()
-        #customers = open('/Users/swarnim/PycharmProjects/data_generation/data/customers.csv', 'r').readlines()
-
-    except:
-        error_msg(1)
-    try:
-        m = str(sys.argv[2])
-        #m = '/Users/swarnim/PycharmProjects/data_generation/profiles/female_30_40_smaller_cities.json'
-        pro_name = m.split('profiles')[-1]
-        pro_name = pro_name[1:]
-        parse_index = m.index('profiles') + 9
-        m_fraud = m[:parse_index] +'fraud_' + m[parse_index:]
-        #m = 'C:\Users\swarnim\PycharmProjects\data_generation\profiles\male_30_40_bigger_cities_fruad.json'
-
-        pro = open(m, 'r').read()
-		
-
-        pro_fraud = open(m_fraud, 'r').read()
-        
-        pro_name_fraud = 'fraud_' + pro_name
-        #fix for windows file paths
-
-
-    except:
-        error_msg(2)
-    try:
-        startd = convert_date(sys.argv[3])
-        #startd = convert_date('01-01-2013')
-    except:
-        error_msg(3)
-    try:
-        endd = convert_date(sys.argv[4])
-        #endd = convert_date('12-31-2014')
-    except:
-        error_msg(4)
-
-
-
-    return customers, pro, pro_fraud, pro_name, pro_name_fraud, startd, endd, m
-
-def create_header(line):
-    headers = line.split('|')
-    headers[-1] = headers[-1].replace('\n','')
-    headers.extend(['trans_num', 'trans_date', 'trans_time','unix_time', 'category', 'amt', 'is_fraud', 'merchant', 'merch_lat', 'merch_long'])
-    print(''.join([h + '|' for h in headers])[:-1])
+def create_header(headers):
+    #     headers = line.split('|')
+    #     headers[-1] = headers[-1].replace('\n','')
+    headers.extend(
+        [
+            "trans_num",
+            "trans_date",
+            "trans_time",
+            "unix_time",
+            "category",
+            "amt",
+            "is_fraud",
+            "merchant",
+            "merch_lat",
+            "merch_long",
+        ]
+    )
+    #     print(''.join([h + '|' for h in headers])[:-1])
     return headers
 
 
 class Customer:
     def __init__(self, customer, profile):
         self.customer = customer
-        self.attrs = self.clean_line(self.customer)
+        #         self.attrs = self.clean_line(self.customer)
         self.fraud_dates = []
 
-    def print_trans(self, trans, is_fraud, fraud_dates):
+    def print_trans(self, trans, is_fraud, fraud_dates, merch, fake):
 
         is_traveling = trans[1]
         travel_max = trans[2]
 
-
+        txn_list = []
 
         for t in trans[0]:
 
             ## Get transaction location details to generate appropriate merchant record
-            cust_state = cust.attrs['state']
-            groups = t.split('|')
+            groups = t.split("|")
             trans_cat = groups[4]
-            merch_filtered = merch[merch['category'] == trans_cat]
-            random_row = merch_filtered.loc[random.sample(list(merch_filtered.index), 1)]
+            merch_filtered = merch[merch["category"] == trans_cat]
+            random_row = merch_filtered.loc[
+                random.sample(list(merch_filtered.index), 1)
+            ]
             ##sw added list
-            chosen_merchant = random_row.iloc[0]['merchant_name']
+            chosen_merchant = random_row.iloc[0]["merchant_name"]
 
-            cust_lat = cust.attrs['lat']
-            cust_long = cust.attrs['long']
-
+            cust_lat = self.customer["lat"]
+            cust_long = self.customer["long"]
+            cust_str = ("|").join(self.customer.values.tolist())
 
             if is_traveling:
                 # hacky math.. assuming ~70 miles per 1 decimal degree of lat/long
                 # sorry for being American, you're on your own for kilometers.
                 rad = (float(travel_max) / 100) * 1.43
 
-                #geo_coordinate() uses uniform distribution with lower = (center-rad), upper = (center+rad)
-                merch_lat = fake.coordinate(center=float(cust_lat),radius=rad)
-                merch_long = fake.coordinate(center=float(cust_long),radius=rad)
+                # geo_coordinate() uses uniform distribution with lower = (center-rad), upper = (center+rad)
+                merch_lat = fake.coordinate(center=float(cust_lat), radius=rad)
+                merch_long = fake.coordinate(center=float(cust_long), radius=rad)
             else:
                 # otherwise not traveling, so use 1 decimial degree (~70mile) radius around home address
                 rad = 1
-                merch_lat = fake.coordinate(center=float(cust_lat),radius=rad)
-                merch_long = fake.coordinate(center=float(cust_long),radius=rad)
+                merch_lat = fake.coordinate(center=float(cust_lat), radius=rad)
+                merch_long = fake.coordinate(center=float(cust_long), radius=rad)
 
             if is_fraud == 0 and groups[1] not in fraud_dates:
-            # if cust.attrs['profile'] == "male_30_40_smaller_cities.json":
-                print(self.customer.replace('\n','') + '|' + t + '|' + str(chosen_merchant) + '|' + str(merch_lat) + '|' + str(merch_long))
+                # if cust.attrs['profile'] == "male_30_40_smaller_cities.json":
+                cust_str = (
+                    cust_str
+                    + "|"
+                    + t
+                    + "|"
+                    + str(chosen_merchant)
+                    + "|"
+                    + str(merch_lat)
+                    + "|"
+                    + str(merch_long)
+                )
+                #                 print(cust_str)
+                txn_list.append(cust_str.split("|"))
 
-            if is_fraud ==1:
-                print(self.customer.replace('\n','') + '|' + t + '|' + str(chosen_merchant) + '|' + str(merch_lat) + '|' + str(merch_long))
+            if is_fraud == 1:
+                cust_str = (
+                    cust_str
+                    + "|"
+                    + t
+                    + "|"
+                    + str(chosen_merchant)
+                    + "|"
+                    + str(merch_lat)
+                    + "|"
+                    + str(merch_long)
+                )
+                #                 print(cust_str)
+                #                 print("=================")
+                txn_list.append(cust_str.split("|"))
 
-            #else:
-                # pass
+        return txn_list
 
-    def clean_line(self, line):
-        # separate into a list of attrs
-        cols = [c.replace('\n','') for c in line.split('|')]
-        # create a dict of name:value for each column
-        attrs = {}
-        for i in range(len(cols)):
-            attrs[headers[i].replace('\n','')] = cols[i].replace('\n','')
-        return attrs
 
-if __name__ == '__main__':
+def generate_transactions(customers, profile_name, start, end):
     # read user input into Inputs object
     # to prepare the user inputs
-    # curr_profile is female_30_40_smaller_cities.json , for fraud as well as non fraud
-    # profile_name is ./profiles/fraud_female_30_40_bigger_cities.json for fraud.
-    customers, pro, pro_fraud, curr_profile, curr_fraud_profile, start, end, profile_name = get_user_input()
-    #if curr_profile == "male_30_40_smaller_cities.json":
-    #   inputCat = "travel"
-    #elif curr_profile == "female_30_40_smaller_cities.json":
-    #    inputCat = "pharmacy"
-    #else:
-    #    inputCat = "misc_net"
 
-    # takes the customers headers and appends
-    # transaction headers and returns/prints
-    #if profile_name[11:][:6] == 'fraud_':
-    # read merchant.csv used for transaction record
-    #    merch = pd.read_csv('./data/merchants_fraud.csv' , sep='|')
-    #else:
-    #    merch = pd.read_csv('./data/merchants.csv', sep='|')
+    base_dir = "./profiles/"
+    headers = create_header(customers.columns.to_list())
 
-    headers = create_header(customers[0])
+    pro = open(base_dir + profile_name, "r").read()
+    pro_fraud = open(base_dir + "fraud_" + profile_name, "r").read()
 
     # generate Faker object to calc merchant transaction locations
     fake = Faker()
 
-
+    txns = pd.DataFrame(columns=headers)
     # for each customer, if the customer fits this profile
     # generate appropriate number of transactions
-    for line in customers[1:]:
-            profile = profile_weights.Profile(pro, start, end)
-            cust = Customer(line, profile)
+    #     for line in customers[1:]:
+    for index, row in customers.iterrows():
+        profile = profile_weights.Profile(pro, start, end)
+        cust = Customer(row, profile)
 
+        if row["profile"] == profile_name:
+            merch = pd.read_csv("data/merchants.csv", sep="|")
+            is_fraud = 0
 
-            if cust.attrs['profile'] == curr_profile:
-                merch = pd.read_csv('data/merchants.csv', sep='|')
-                is_fraud= 0
+            fraud_flag = randint(
+                0, 100
+            )  # set fraud flag here, as we either gen real or fraud, not both for
+            # the same day.
+            fraud_dates = []
 
-                fraud_flag = randint(0,100) # set fraud flag here, as we either gen real or fraud, not both for
-                                        # the same day.
-                fraud_dates = []
-
-
-                # decide if we generate fraud or not
-                if fraud_flag < 99: #11->25
-                    fraud_interval = randint(1,1) #7->1
-                    inter_val = (end-start).days-7
-                    # rand_interval is the random no of days to be added to start date
-                    rand_interval = randint(1, inter_val)
-                    #random start date is selected
-                    newstart = start + datetime.timedelta(days=rand_interval)
-                    # based on the fraud interval , random enddate is selected
-                    newend = newstart + datetime.timedelta(days=fraud_interval)
-                    # we assume that the fraud window can be between 1 to 7 days #7->1
-                    profile = profile_weights.Profile(pro_fraud, newstart, newend)
-                    cust = Customer(line, profile)
-                    merch = pd.read_csv('data/merchants.csv' , sep='|')
-                    is_fraud = 1
-                    temp_tx_data = profile.sample_from(is_fraud)
-                    fraud_dates = temp_tx_data[3]
-                    cust.print_trans(temp_tx_data,is_fraud, fraud_dates)
-                    #parse_index = m.index('profiles/') + 9
-                    #m = m[:parse_index] +'fraud_' + m[parse_index:]
-
-                # we're done with fraud (or didn't do it) but still need regular transactions
-                # we pass through our previously selected fraud dates (if any) to filter them
-                # out of regular transactions
-                profile = profile_weights.Profile(pro, start, end)
-                merch = pd.read_csv('data/merchants.csv', sep='|')
-                is_fraud =0
+            # decide if we generate fraud or not
+            if fraud_flag < 99:  # 11->25
+                fraud_interval = randint(1, 1)  # 7->1
+                inter_val = (end - start).days - 7
+                # rand_interval is the random no of days to be added to start date
+                rand_interval = 1 if inter_val <= 0 else randint(1, inter_val)
+                # random start date is selected
+                newstart = start + timedelta(days=rand_interval)
+                # based on the fraud interval , random enddate is selected
+                newend = newstart + timedelta(days=fraud_interval)
+                # we assume that the fraud window can be between 1 to 7 days #7->1
+                profile = profile_weights.Profile(pro_fraud, newstart, newend)
+                cust = Customer(row, profile)
+                merch = pd.read_csv("data/merchants.csv", sep="|")
+                is_fraud = 1
                 temp_tx_data = profile.sample_from(is_fraud)
-                cust.print_trans(temp_tx_data, is_fraud, fraud_dates)
+                fraud_dates = temp_tx_data[3]
 
+                cust_txns = cust.print_trans(
+                    temp_tx_data, is_fraud, fraud_dates, merch, fake
+                )
+                cust_txns_df = pd.DataFrame(cust_txns, columns=headers)
+                txns = txns.append(cust_txns_df, ignore_index=True)
+                # parse_index = m.index('profiles/') + 9
+                # m = m[:parse_index] +'fraud_' + m[parse_index:]
 
+            # we're done with fraud (or didn't do it) but still need regular transactions
+            # we pass through our previously selected fraud dates (if any) to filter them
+            # out of regular transactions
+            profile = profile_weights.Profile(pro, start, end)
+            merch = pd.read_csv("data/merchants.csv", sep="|")
+            is_fraud = 0
+            temp_tx_data = profile.sample_from(is_fraud)
+            cust_txns = cust.print_trans(
+                temp_tx_data, is_fraud, fraud_dates, merch, fake
+            )
+            cust_txns_df = pd.DataFrame(cust_txns, columns=headers)
+            txns = txns.append(cust_txns_df, ignore_index=True)
 
+    return txns
--- main_config.py
+++ main_config.py
@@ -1,18 +1,17 @@
 import json
 
-class MainConfig:
 
+class MainConfig:
     def __init__(self, main):
         self.config = self.all_profiles_dicts(main)
-
 
     # convert type to a tuple
     def convert_config_type(self, x):
         if type(x) is dict:
-            minval = float(x['min'])
-            maxval = float(x['max'])
+            minval = float(x["min"])
+            maxval = float(x["max"])
             if maxval < 0:
-                return (minval, float('inf'))
+                return (minval, float("inf"))
             else:
                 return (minval, maxval)
         else:
@@ -20,42 +19,41 @@ class MainConfig:
 
     def all_profiles_dicts(self, main):
 
-        main_config = json.loads(json.dumps(main).\
-            replace('\\n','').\
-            replace('\\t','').\
-            replace('\\','').\
-            replace('"{','{').\
-            replace('}"','}'))	
+        main_config = json.loads(
+            json.dumps(main)
+            .replace("\\n", "")
+            .replace("\\t", "")
+            .replace("\\", "")
+            .replace('"{', "{")
+            .replace('}"', "}")
+        )
 
         all_profiles = {}
         for pf in main_config:
-            if pf != 'leftovers.json':
+            if pf != "leftovers.json":
                 all_profiles[pf] = {}
                 for qual in main_config[pf]:
-                    all_profiles[pf][qual] = \
-                    self.convert_config_type(main_config[pf][qual])
+                    all_profiles[pf][qual] = self.convert_config_type(
+                        main_config[pf][qual]
+                    )
 
         return all_profiles
 
     def in_profile(self, person_dict, profile_quals):
         for pq in profile_quals:
-            if pq == 'age':
-                if self.fits_qual(return_age(person_dict),\
-                             profile_quals[pq]) == False:
+            if pq == "age":
+                if self.fits_qual(return_age(person_dict), profile_quals[pq]) == False:
                     return False
             else:
-                if self.fits_qual(person_dict[pq],\
-                             profile_quals[pq]) == False:
+                if self.fits_qual(person_dict[pq], profile_quals[pq]) == False:
                     return False
         return True
 
-
     def fits_qual(self, person_val, range_tuple):
         if type(range_tuple) is list:
-            # matching value in string list 
+            # matching value in string list
             # (e.g. ['M','F'])
-            if unicode(person_val) in range_tuple or \
-                str(person_val) in range_tuple:
+            if unicode(person_val) in range_tuple or str(person_val) in range_tuple:
                 return True
             # doesn't match
             else:
@@ -69,8 +67,9 @@ class MainConfig:
                 return False
 
         # range
-        if float(person_val) >= float(range_tuple[0]) and\
-            float(person_val) <= float(range_tuple[1]):
-                return True
+        if float(person_val) >= float(range_tuple[0]) and float(person_val) <= float(
+            range_tuple[1]
+        ):
+            return True
         else:
-            return False        
+            return False
